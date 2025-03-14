280397b9c303a9e0ce904f0eda31f71de341b769
Rampardos <63419320+BarbatosV2@users.noreply.github.com>
2025-03-14 11:01:29 +1100

    added support role yone

--- javascript/content.js
+++ javascript/content.js
@@ -59,8 +59,14 @@ function showContent(imageId) {
         case 'image5':
             newContent.innerHTML = `
                 <h2 class="title">Support</h2>
-                <p class="para">You don't want to see me play this role.</p>
-                <p class="para">Quite the opposite of ADC/APC(Bot), I will take all the CS :)</p>`;
+                <div class="champ-select">
+                <label for="model5">Choose a Champion:</label>
+                <select id="modelSelector5">
+                    <option value="ocean_song_yone.glb" selected>Yone</option>
+                </select>
+                </div>
+                <div id="modelViewerContainer5"></div>
+                <div id="paragraphContainer5" class="paragraph-container5"></div>`;
             break;
         default:
             console.log('Image not found');
@@ -77,6 +83,7 @@ function attachModelSelectListeners() {
     const modelSelector1 = document.getElementById('modelSelector1');
     const modelSelector2 = document.getElementById('modelSelector2');
     const modelSelector3 = document.getElementById('modelSelector3');
+    const modelSelector5 = document.getElementById('modelSelector5');
 
     if (modelSelector1) {
         modelSelector1.addEventListener('change', showSelectedModelContent);
@@ -87,12 +94,16 @@ function attachModelSelectListeners() {
     if (modelSelector3) {
         modelSelector3.addEventListener('change', showSelectedModelContent);
     }
+    if (modelSelector5) {
+        modelSelector5.addEventListener('change', showSelectedModelContent);
+    }
 }
 
 function showSelectedModelContent() {
     const modelSelector1 = document.getElementById('modelSelector1');
     const modelSelector2 = document.getElementById('modelSelector2');
     const modelSelector3 = document.getElementById('modelSelector3');
+    const modelSelector5 = document.getElementById('modelSelector5');
 
     if (modelSelector1) {
         const selectedModel = modelSelector1.value;
@@ -938,21 +949,21 @@ function showSelectedModelContent() {
                         <div class="rune-row">
                             <img class="rune-image" src="Runes/Resolve/Resolve_icon.webp">
                         </div>
-                    <div class="rune-row">
-                        <img class="rune-image rune-unselect" src="Runes/Resolve/Demolish_rune.webp">
-                        <img class="rune-image rune-unselect" src="Runes/Resolve/Font_of_Life_rune.webp">
-                        <img class="rune-image rune-unselect" src="Runes/Resolve/Shield_Bash_rune.webp">
-                    </div>
-                    <div class="rune-row">
-                        <img class="rune-image rune-unselect" src="Runes/Resolve/Conditioning_rune.webp">
-                        <img class="rune-image" src="Runes/Resolve/Second_Wind_rune.webp">
-                        <img class="rune-image rune-unselect" src="Runes/Resolve/Bone_Plating_rune.webp">
-                    </div>
-                    <div class="rune-row">
-                        <img class="rune-image rune-unselect" src="Runes/Resolve/Overgrowth_rune.webp">
-                        <img class="rune-image" src="Runes/Resolve/Revitalize_rune.webp">
-                        <img class="rune-image rune-unselect" src="Runes/Resolve/Unflinching_rune.webp">
-                    </div>
+                        <div class="rune-row">
+                            <img class="rune-image rune-unselect" src="Runes/Resolve/Demolish_rune.webp">
+                            <img class="rune-image rune-unselect" src="Runes/Resolve/Font_of_Life_rune.webp">
+                            <img class="rune-image rune-unselect" src="Runes/Resolve/Shield_Bash_rune.webp">
+                        </div>
+                        <div class="rune-row">
+                            <img class="rune-image rune-unselect" src="Runes/Resolve/Conditioning_rune.webp">
+                            <img class="rune-image" src="Runes/Resolve/Second_Wind_rune.webp">
+                            <img class="rune-image rune-unselect" src="Runes/Resolve/Bone_Plating_rune.webp">
+                        </div>
+                        <div class="rune-row">
+                            <img class="rune-image rune-unselect" src="Runes/Resolve/Overgrowth_rune.webp">
+                            <img class="rune-image" src="Runes/Resolve/Revitalize_rune.webp">
+                            <img class="rune-image rune-unselect" src="Runes/Resolve/Unflinching_rune.webp">
+                        </div>
                     </div>
                     <h3 class="title">Shards</h3>
                     <div class="rune-container">
@@ -2054,6 +2065,199 @@ function showSelectedModelContent() {
                 modelViewerContainer3.innerHTML = '';
                 // Clear paragraphs if no model matches
                 paragraphContainer3.innerHTML = '';
+                break;
+        }
+    }
+    if (modelSelector5) {
+        const selectedModel = modelSelector5.value;
+        const modelViewerContainer5 = document.getElementById('modelViewerContainer5');
+        const paragraphContainer5 = document.getElementById('paragraphContainer5');
+        modelViewerContainer5.innerHTML = ''; // Clear model viewer container content
+        switch (selectedModel) {
+            case 'ocean_song_yone.glb':
+                // Set model viewer container content for Malzahar
+                modelViewerContainer5.innerHTML = `
+                    <model-viewer src="3dAnimated/${selectedModel}" 
+                    alt="Model" ar ar-modes="webxr scene-viewer quick-look" 
+                    camera-controls 
+                    animation-name="Run_Homeguard" autoplay
+                    camera-controls camera-orbit="350deg 80deg" field-of-view="40deg">
+                    </model-viewer>`;
+                paragraphContainer5.innerHTML = `
+                    <h2 class="champ-name">Yone</h2>
+                    <p class="para">I play Yone Support for fun</p>
+                    <p class="para">Control ward doesn't do damage but Infinity Edge does ;)</p>
+                    <h2 class="title">Priority</h2>
+                    <div class="map-container">
+                    <img src="map/Bot/LoLMap_Prio.png" alt="LoLMap_Prio">
+                    </div>
+                    <h2 class="title">Counter</h2>
+                    <div class="map-container">
+                    <img src="map/Bot/LoLMap_Counter.png" alt="LoLMap_Counter">
+                    </div>
+                    <h2 class="title">Spells</h2>
+                        <h3 class="title">lvl 1 - 18</h3>
+                        <div class="spells">
+                        <div class="spell-image">
+                            <img src="ChampSpells/Yone/Q.webp" alt="Q">
+                            <div class="label">Q</div>
+                        </div>
+                        <div class="spell-image">
+                            <img src="ChampSpells/Yone/W.webp" alt="W">
+                            <div class="label">W</div>
+                        </div>
+                        <div class="spell-image">
+                            <img src="ChampSpells/Yone/E.webp" alt="E">
+                            <div class="label">E</div>
+                        </div>
+                        <div class="spell-image">
+                            <img src="ChampSpells/Yone/Q.webp" alt="Q">
+                            <div class="label">Q</div>
+                        </div>
+                        <div class="spell-image">
+                            <img src="ChampSpells/Yone/Q.webp" alt="Q">
+                            <div class="label">Q</div>
+                        </div>
+                        <div class="spell-image">
+                            <img src="ChampSpells/Yone/R.webp" alt="R">
+                            <div class="label">R</div>
+                        </div>
+                        <div class="spell-image">
+                            <img src="ChampSpells/Yone/Q.webp" alt="Q">
+                            <div class="label">Q</div>
+                        </div>
+                        <div class="spell-image">
+                            <img src="ChampSpells/Yone/W.webp" alt="W">
+                            <div class="label">W</div>
+                        </div>
+                        <div class="spell-image">
+                            <img src="ChampSpells/Yone/Q.webp" alt="Q">
+                            <div class="label">Q</div>
+                        </div>
+                        <div class="spell-image">
+                            <img src="ChampSpells/Yone/W.webp" alt="W">
+                            <div class="label">W</div>
+                        </div>
+                        <div class="spell-image">
+                            <img src="ChampSpells/Yone/R.webp" alt="R">
+                            <div class="label">R</div>
+                        </div>
+                        <div class="spell-image">
+                            <img src="ChampSpells/Yone/W.webp" alt="W">
+                            <div class="label">W</div>
+                        </div>
+                        <div class="spell-image">
+                            <img src="ChampSpells/Yone/E.webp" alt="E">
+                            <div class="label">E</div>
+                        </div>
+                        <div class="spell-image">
+                            <img src="ChampSpells/Yone/E.webp" alt="E">
+                            <div class="label">E</div>
+                        </div>
+                        <div class="spell-image">
+                            <img src="ChampSpells/Yone/E.webp" alt="E">
+                            <div class="label">E</div>
+                        </div>
+                        <div class="spell-image">
+                            <img src="ChampSpells/Yone/R.webp" alt="R">
+                            <div class="label">R</div>
+                        </div>
+                        <div class="spell-image">
+                            <img src="ChampSpells/Yone/E.webp" alt="E">
+                            <div class="label">E</div>
+                        </div>
+                        <div class="spell-image">
+                            <img src="ChampSpells/Yone/E.webp" alt="E">
+                            <div class="label">E</div>
+                        </div>
+    
+                        <!-- Repeat the above structure for other images -->
+                        <!-- Add more <div class="spell-image"> elements for each image -->
+                    </div>
+
+                    <h2 class="title">Runes</h2>
+                        <h3 class="title">Primary Rune</h3>
+                        <div class="rune-container">
+                            <div class="rune-row">
+                                <img class="rune-image" src="Runes/Precision/Precision_icon.webp">
+                            </div>
+                            <div class="rune-main">
+                                <img class="rune-image rune-unselect" src="Runes/Precision/Press_the_Attack_rune.webp">
+                                <img class="rune-image" src="Runes/Precision/Lethal_Tempo_rune.webp">
+                                <img class="rune-image rune-unselect" src="Runes/Precision/Fleet_Footwork_rune.webp">
+                                <img class="rune-image rune-unselect" src="Runes/Precision/Conqueror_rune.webp">
+                            </div>
+                            <div class="rune-row">
+                                <img class="rune-image rune-unselect" src="Runes/Precision/Overheal_rune.webp">
+                                <img class="rune-image" src="Runes/Precision/Triumph_rune.webp">
+                                <img class="rune-image rune-unselect" src="Runes/Precision/Presence_of_Mind_rune.webp">
+                            </div>
+                            <div class="rune-row">
+                                <img class="rune-image" src="Runes/Precision/Legend-_Alacrity_rune.webp">
+                                <img class="rune-image rune-unselect" src="Runes/Precision/Legend-_Tenacity_rune.webp">
+                                <img class="rune-image rune-unselect" src="Runes/Precision/Legend-_Bloodline_rune.webp">
+                            </div>
+                            <div class="rune-row">
+                                <img class="rune-image rune-unselect" src="Runes/Precision/Coup_de_Grace_rune.webp">
+                                <img class="rune-image rune-unselect" src="Runes/Precision/Cut_Down_rune.webp">
+                                <img class="rune-image" src="Runes/Precision/Last_Stand_rune.webp">
+                            </div>
+                        </div>
+
+                    <h3 class="title">Secondary Rune</h3>
+                    <div class="rune-container">
+                        <div class="rune-row">
+                            <img class="rune-image" src="Runes/Resolve/Resolve_icon.webp">
+                        </div>
+                        <div class="rune-row">
+                            <img class="rune-image rune-unselect" src="Runes/Resolve/Demolish_rune.webp">
+                            <img class="rune-image rune-unselect" src="Runes/Resolve/Font_of_Life_rune.webp">
+                            <img class="rune-image rune-unselect" src="Runes/Resolve/Shield_Bash_rune.webp">
+                        </div>
+                        <div class="rune-row">
+                            <img class="rune-image rune-unselect" src="Runes/Resolve/Conditioning_rune.webp">
+                            <img class="rune-image" src="Runes/Resolve/Second_Wind_rune.webp">
+                            <img class="rune-image rune-unselect" src="Runes/Resolve/Bone_Plating_rune.webp">
+                        </div>
+                        <div class="rune-row">
+                            <img class="rune-image rune-unselect" src="Runes/Resolve/Overgrowth_rune.webp">
+                            <img class="rune-image" src="Runes/Resolve/Revitalize_rune.webp">
+                            <img class="rune-image rune-unselect" src="Runes/Resolve/Unflinching_rune.webp">
+                        </div>
+                    </div>
+                    <h3 class="title">Shards</h3>
+                    <div class="rune-container">
+                        <div class="rune-row">
+                            <img class="rune-image rune-unselect" src="Runes/Shards/Adaptive_Force.webp">
+                            <img class="rune-image" src="Runes/Shards/Attack_Speed.webp">
+                            <img class="rune-image rune-unselect" src="Runes/Shards/Cooldown_Reduction.webp">
+                        </div>
+                        <div class="rune-row">
+                            <img class="rune-image" src="Runes/Shards/Adaptive_Force.webp">
+                            <img class="rune-image rune-unselect" src="Runes/Shards/Movement_Speed.webp">
+                            <img class="rune-image rune-unselect" src="Runes/Shards/Health_Scaling.webp">
+                        </div>
+                        <div class="rune-row">
+                            <img class="rune-image" src="Runes/Shards/Health.webp">
+                            <img class="rune-image rune-unselect" src="Runes/Shards/Tenacity_and_Slow_Resist.webp">
+                            <img class="rune-image rune-unselect" src="Runes/Shards/Health_Scaling.webp">
+                        </div>
+                    </div>
+                    <h2 class="title">Items</h2>
+                    <h3 class="title">Initial State</h3>
+                    <div class="rune-row">
+                        <img class="rune-image item" src="items/bot/World_Atlas.webp">
+                        <img class="rune-image item" src="items/potions/Health_Potion.webp">
+                        <img class="rune-image item" src="items/potions/Health_Potion.webp">
+                    </div>`;
+                break;
+            
+            // Add cases for other models here
+            default:
+                // Clear model viewer container content if no model matches
+                modelViewerContainer5.innerHTML = '';
+                // Clear paragraphs if no model matches
+                paragraphContainer5.innerHTML = '';
                 break;
         }
     }
